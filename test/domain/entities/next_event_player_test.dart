6475dbd79a1f17294a1b3371c067f6655bd0459e
Lucas Santos <lucas201273@gmail.com>
2025-07-06 19:28:39 -0300

    test: return correct initials in a name with 2 words

--- test/domain/entities/next_event_player_test.dart
+++ test/domain/entities/next_event_player_test.dart
@@ -19,17 +19,24 @@ class NextEventPlayer {
 
   String getInitials(){
     final  names = name.split(' ');
-    return '${names[0][0]}${names[1][0]}';
+    final firstChar = names.first[0];
+    final lastChar = names.last[0];
+    return '$firstChar$lastChar';
   }
 }
 
 void main() {
-  test('should returns the first lettter of the first and last name', () {
+  test('should returns the first letter of the first and last name', () {
     final player = NextEventPlayer(
       id: '',
       name: 'Lucas Santos',
       isConfirmed: true,
     );
     expect(player.getInitials(), equals('LS'));
+
+    final player2 = NextEventPlayer(id: '', name: 'Paulo Lima', isConfirmed: false);
+    expect(player2.getInitials(), equals('PL'));
+
+
   });
 }
